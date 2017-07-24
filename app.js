d887eca186b22a5b73c98e8695e80f04d40f7718
Dylan Dunne <dylanadunne@gmail.com>
2017-07-23 22:12:32 -0400

    added area calculator

--- app.js
+++ app.js
@@ -3,15 +3,24 @@
 function draw() {
     var userInput = document.getElementById('circle-rad');
     var radius = userInput.value;
-    var draw = SVG('drawing').size(300, 130);
+    var draw = SVG('drawing').size(250, 250);
     var circ = draw.circle(radius).fill('#f06');
-
+    circleDescription();
+    findArea(radius);
 }
 
+function circleDescription() {
+    var userInput = document.getElementById('circle-rad');
+    var radius = userInput.value;
+}
 
+function findArea(rad) {
+    var outputList = document.getElementById('output-list');
+    var output = document.createElement('li');
+    var area = (2 * rad) * 3.14159;
+    output.innerHTML = 'Circle Area = ' + area + ' pixels!';
+    outputList.appendChild(output);
+    console.log(area);
+}
 
-
-// Calculate area of circle
-// Add animations
 // Improve design
-// Yield some output to the page
